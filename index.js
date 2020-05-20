f6b3fe428c2bdea917cdb81ed4eedcd00caef346
Piotr Bartnicki <pbartnicki@carltonone.com>
2020-05-20 16:44:11 -0400

    proxy: Fix 1

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var proxy = require('express-http-proxy');
 var app = require('express')();
+var port = 3000
 //fix ssl localhost
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 app.use('/market', proxy('https://localhost:444'));
 app.use('/sso', proxy('https://localhost:443'))
-app.listen(3000);
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
